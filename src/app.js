3796f50e6e6ec30563820a17b9b0ee3161e5df82
Joé Jemmely <joe.jemmely@gmail.com>
2019-05-28 13:30:51 -0700

    Load book routes

--- src/app.js
+++ src/app.js
@@ -6,9 +6,12 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 // Esoteric Resources
-const errorHandler = require( './middleware/error.js');
-const notFound = require( './middleware/404.js' );
-const authRouter = require( './auth/router.js' );
+const errorHandler = require('./middleware/error.js');
+const notFound = require('./middleware/404.js');
+const authRouter = require('./auth/router.js');
+
+// Routes
+const bookRoutes = require('./routes/books.js');
 
 // Prepare the express app
 const app = express();
@@ -18,7 +21,9 @@ app.use(cors());
 app.use(morgan('dev'));
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', bookRoutes);
 
 // Catchalls
 app.use(notFound);
@@ -28,14 +33,13 @@ let isRunning = false;
 
 module.exports = {
   server: app,
-  start: (port) => {
-    if( ! isRunning ) {
+  start: port => {
+    if (!isRunning) {
       app.listen(port, () => {
         isRunning = true;
         console.log(`Server Up on ${port}`);
       });
-    }
-    else {
+    } else {
       console.log('Server is already running');
     }
   },
